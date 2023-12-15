d281020114ba0a88e2e2a4921a1a0af771d051b3
Nora Velde <noravelde@gmail.com>
2023-12-15 14:35:27 +0100

    added element to friends route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import FriendsPage from "./Pages/FriendsPage";
 
 function App() {
   return <div className="App">
     <Routes>
-      <Route path="/"/>
+      <Route path="/" />
       <Route path="/tasks"/>
       <Route path="/habits" />
-      <Route path="/friends" />
+      <Route path="/friends" element={FriendsPage} />
     </Routes>
   </div>;
 }
