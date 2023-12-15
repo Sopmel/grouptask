85db712d9f8e9b299df23817f677c3a03db8749b
Elin Östman <elin.ostman@hotmail.com>
2023-12-15 14:36:24 +0100

    Lagt till element för routes Homepage och Habitspage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import HabitsPage from "./Pages/HabitsPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/"/>
-      <Route path="/tasks"/>
-      <Route path="/habits" />
-      <Route path="/friends" />
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tasks" />
+        <Route path="/habits" element={<HabitsPage />} />
+        <Route path="/friends" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/HabitsPage.jsx
+++ src/Pages/HabitsPage.jsx
@@ -6,4 +6,4 @@ function HabitsPage() {
   )
 }
 
-export default HabitsPage
+export default HabitsPage;
