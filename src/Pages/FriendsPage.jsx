21d32096c06b8cbd60cb95c6581ed0a51479b8e8
Nora Velde <noravelde@gmail.com>
2023-12-19 13:22:26 +0100

    added filter

--- src/Pages/FriendsPage.jsx
+++ src/Pages/FriendsPage.jsx
@@ -1,8 +1,13 @@
 import FriendsProfile from "../Components/FriendsProfile"
+import FilterFriends from "../Components/filterFriends"
 import { useEffect, useState } from "react"
+
 
 const FriendsPage = () => {
     const [friends, setFriends] = useState([])
+    const [filteredFriends, setFilteredFriends] = useState([])
+    // const [filteredFriends, setFilteredFriends] = useState([])
+    // const [usingFilter, setUsingFilter] = useState(false)
 
     const addFriend = (user) => {
         setFriends([...friends,
@@ -10,7 +15,8 @@ const FriendsPage = () => {
             name: `${user.results[0].name.title} ${user.results[0].name.first} ${user.results[0].name.last}`,
             img: `${user.results[0].picture.thumbnail}`,
             email: `${user.results[0].email}`,
-            dateOfBirth: `${user.results[0].dob.date}`,
+            dob: `${user.results[0].dob.date}`,
+            age: `${user.results[0].dob.age}`,
             gender: `${user.results[0].gender}`,
             id: friends.length + 1,
         },
@@ -23,6 +29,10 @@ const FriendsPage = () => {
         addFriend(json)
     }
 
+    const addFilter = (data) => {
+        setFilteredFriends(data)
+    }
+
     useEffect(() => {
         fetchUserData()
     }, [])
@@ -30,18 +40,32 @@ const FriendsPage = () => {
     return (
         <div>
             <div>
+                <div>
+                    <FilterFriends friends={friends} setFilteredFriends={addFilter} />
+                    {
+                        filteredFriends && filteredFriends.map((user, index)=> {
+                            return(
+                                <FriendsProfile key={index} user={user}/>
+                            )
+                        })
+                    }
+                </div>
+
+                <div>
                 <h2>Friends:</h2>
                 <h3>{friends.length} st</h3>
-                {friends && friends.map((user) => {
+                    {friends && friends.map((user, index) => {
                     return (
-                        <FriendsProfile user={user} />
+                        <FriendsProfile key={index} user={user} />
                     )
                 })}
+                </div>
+                
             </div>
+
 
             <button onClick={ () => { 
                 fetchUserData() 
-                console.log(friends)
             }}
             >Add Friend</button>
         </div>
