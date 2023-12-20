851266648f375e2ddd09369fffd284bb3a4c6186
Nora Velde <noravelde@gmail.com>
2023-12-20 11:37:43 +0100

    Added sorting and fixed filter

--- src/Components/filterFriends.jsx
+++ src/Components/filterFriends.jsx
@@ -3,14 +3,17 @@ import FriendsProfile from "./FriendsProfile"
 import styling from "./FilterFriendsStyle.module.css"
 
 const FilterFriends = ( props ) => {
-    const [gender, setGender] = useState('')
-    // const [filteredFriends, setFilteredFriends] = useState(friends)
+  
     const {setFilteredFriends, friends} = props
 
-    const minAgeInArray = Math.min(...friends.map(user => user.age));
-    const maxAgeInArray = Math.max(...friends.map(user => user.age))
-    const [maxAge, setMaxAge] = useState(maxAgeInArray.toString())
-    const [minAge, setMinAge] = useState(minAgeInArray.toString())
+    //Varning något om att ett infinite värde inte kan användas? 
+    // const minAgeInArray = Math.min(...friends.map(user => user.age));
+    // const maxAgeInArray = Math.max(...friends.map(user => user.age))
+    // const [maxAge, setMaxAge] = useState(maxAgeInArray.toString())
+    // const [minAge, setMinAge] = useState(minAgeInArray.toString())
+    const [gender, setGender] = useState('')
+    const [minAge, setMinAge] = useState('')
+    const [maxAge, setMaxAge] = useState('');
 
 
     const handleFilter = () => {
@@ -42,41 +45,31 @@ const FilterFriends = ( props ) => {
                 <label>
                     Max Age:
                     <input
-                        type="range"
-                        min={minAgeInArray ? minAgeInArray : 0}
-                        max={maxAgeInArray ? maxAgeInArray : 100}
+                        type="number"
+                        // min={minAgeInArray ? minAgeInArray : 0}
+                        // max={maxAgeInArray ? maxAgeInArray : 100}
                         value={maxAge}
                         onChange={e => setMaxAge(e.target.value)}
-                        // e => setMaxAge(e.target.value)
                     />
-                    {maxAge && <span>{maxAge}</span>}
+                    {/* {maxAge && <span>{maxAge}</span>} */}
                 </label>
 
                 <label>
                     Minimun Age:
                     <input
-                        type="range"
-                        min={minAgeInArray ? minAgeInArray : 0}
-                        max={maxAgeInArray ? maxAgeInArray : 100}
+                        type="number"
+                        // min={minAgeInArray ? minAgeInArray : 0}
+                        // max={maxAgeInArray ? maxAgeInArray : 100}
                         value={minAge}
                         onChange={e => setMinAge(e.target.value)}
                     />
-                    {minAge & <span>{minAge}</span>}
+                    {/* {minAge & <span>{minAge}</span>} */}
                 </label>
 
                 <button onClick={handleFilter}>
                     Apply filter
                 </button>
             </div>
-
-            {/* <div>
-                <p>Filtered Friends</p>
-                <div>
-                    {filteredFriends.map((user, index) =>
-                        <FriendsProfile user={user} key={index} />
-                    )}
-                </div>
-            </div> */}
         </div>
     )
 }
