a539b8273ba4e81d80fe5f1aa087ae0ca2619ade
Nora Velde <noravelde@gmail.com>
2023-12-21 19:35:01 +0100

    fixed homepage friendsList

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
+import FriendsList from '../Components/FriendsList';
 
 function HomePage() {
+  const [latestFriends, setLatestFriends] = useState([]);
+  const location = useLocation();
+
+  useEffect(()=>{
+    
+    console.log(location.state)
+    renderFiveFriends(location.state)
+  }, [])
+
+  const renderFiveFriends = (arr) => {
+    let slicedArray = arr.slice(Math.max(arr.length - 5, 0))
+    setLatestFriends(slicedArray)
+  } 
+  
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <FriendsList friends={latestFriends} />
+      <Link to="/friends" state={renderFiveFriends} >
+        see Friends
+      </Link>
+    </>
   )
 }
 
