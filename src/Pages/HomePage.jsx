22d61ff3f5b9ec37251192e42b8c1a5c892c3dd0
Nora Velde <noravelde@gmail.com>
2023-12-27 15:07:18 +0100

    fixed bug

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,48 +6,54 @@ import TopHabits from '../Components/TopHabits';
 
 function HomePage() {
   const [latestFriends, setLatestFriends] = useState([]);
-  const [ topHabits, setTopHabits ] = useState([]);
- 
+  const [topHabits, setTopHabits] = useState([]);
+
   const location = useLocation();
   console.log(location.state);
-  
-   
-  useEffect(()=>{
+
+  useEffect(() => {
     console.log(location.state)
-    renderFiveFriends(location.state)
+    {location.state && 
+    renderFiveFriends(location.state)}
+
+    // renderTopHabits(location.state)
+  
   }, [])
 
   const renderFiveFriends = (arr) => {
-    let slicedArray = arr.slice(Math.max(arr.length - 5, 0))
+    let sortedArray = [...arr].sort((a, b)=> {
+      return a.id - b.id 
+    })
+    let slicedArray = sortedArray.slice(Math.max(arr.length - 5, 0))
     setLatestFriends(slicedArray)
-  } 
+  }
 
   // HABITS
-  useEffect(()=>{
-    
-    renderTopHabits(location.state)
-  }, [])
+  // useEffect(()=>{
+
+  //   renderTopHabits(location.state)
+  // }, [])
 
   const renderTopHabits = (topH) => {
     console.log(topH);
     let slicedArrayH = topH.slice(Math.max(topH.length - 3, 0))
     setTopHabits(slicedArrayH);
-  } 
- 
+  }
+
 
   return (
     <>
-      <div style={{backgroundColor: "#1c5456", paddingBottom: "2rem"}}>HomePage
-      <FriendsList friends={latestFriends} />
-      <Link to="/friends" state={{ friends: renderFiveFriends }} >
-        see Friends
-      </Link>
-      <hr />
-      <h3 style={{color: "#ffffff"}}>Top 3 Habits</h3>
-      <TopHabits habits={topHabits}/>
-      <Link style={{color: "#ffffff"}} to="/habits" state={{habits: renderTopHabits}} >
-        see all Habits
-      </Link>
+      <div style={{ backgroundColor: "#1c5456", paddingBottom: "2rem" }}>HomePage
+        <FriendsList friends={latestFriends} />
+        <Link to="/friends" state={{ friends: renderFiveFriends }} >
+          see Friends
+        </Link>
+        <hr />
+        <h3 style={{ color: "#ffffff" }}>Top 3 Habits</h3>
+        <TopHabits habits={topHabits} />
+        <Link style={{ color: "#ffffff" }} to="/habits" state={{ habits: renderTopHabits }} >
+          see all Habits
+        </Link>
       </div>
     </>
   )
