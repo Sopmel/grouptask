006b1bc8f88efcf816ab8f9847d770dfe87362f9
Nora Velde <noravelde@gmail.com>
2023-12-28 15:43:35 +0100

    tog bort utkommenteradkod

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -25,7 +25,6 @@ function HomePage(props) {
     setTopHabits(slicedArrayH);
   } 
 
-
   return (
     <>
       <div style={{ backgroundColor: "#1c5456", paddingBottom: "2rem" }}>HomePage
@@ -37,51 +36,5 @@ function HomePage(props) {
     </>
   )
 }
-
-// function HomePage(props) {
-//   const [latestFriends, setLatestFriends] = useState([]);
-//   const [ topHabits, setTopHabits ] = useState([]);
-
-//   const location = useLocation();
-//   console.log(location.state);
-
-
-//   useEffect(()=>{
-//     console.log(location.state)
-//     renderFiveFriends(props.friends)
-//   }, [])
-
-//   const renderFiveFriends = (arr) => {
-//     let sortedArray = [...arr].sort((a, b)=> { return a.id - b.id })
-//     let slicedArray = sortedArray.slice(Math.max(arr.length - 5, 0))
-//     setLatestFriends(slicedArray)
-//   } 
-
-//   // HABITS
-//   useEffect(()=>{
-
-//     renderTopHabits(location.state)
-//   }, [])
-
-//   const renderTopHabits = (topH) => {
-//     console.log(topH);
-//     let slicedArrayH = topH.slice(Math.max(topH.length - 3, 0))
-//     setTopHabits(slicedArrayH);
-//   } 
-
-//   return (
-//     <>
-//       <div style={{backgroundColor: "#1c5456", paddingBottom: "2rem"}}>HomePage
-//       <FriendsList friends={latestFriends} />
-//       <hr />
-//       <h3 style={{color: "#ffffff"}}>Top 3 Habits</h3>
-//       <TopHabits habits={topHabits}/>
-//       <Link style={{color: "#ffffff"}} to="/habits" state={{habits: renderTopHabits}} >
-//         see all Habits
-//       </Link>
-//       </div>
-//     </>
-//   )
-// }
 
 export default HomePage;
